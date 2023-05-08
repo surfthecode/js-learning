0b6981ff74eb36e0f41a7d5f718e2f511e4a6127
surfthecode <surfthecode@gmail.com>
2023-05-08 13:08:58 +0300

    initial commit

---
+++ script-ex3.js
@@ -0,0 +1,16 @@
+/* -------------------------------------------- */
+/*                compara A cu B                */
+/* -------------------------------------------- */
+const a = 5;
+const b = 6;
+
+if (a > b) console.log("A este mai mare decat B");
+else console.log("B este mai mare sau egal cu A");
+
+/* -------------------------------------------- */
+/*                compara varsta                */
+/* -------------------------------------------- */
+const varsta = 20;
+console.log(varsta >= 18 ? "Can vote" : "Can't vote");
+
+console.log("----------------------------------------");
---
+++ script-ex4.js
@@ -0,0 +1,14 @@
+/* -------------------------------------------- */
+/*     2 conditii adevarate sau false cu &&     */
+/* -------------------------------------------- */
+const num = 5;
+console.log(
+  "X este un numar mai mare sau egal cu 5:",
+  typeof num === "number" && num >= 5
+);
+
+/* -------------------------------------------- */
+/*       conditie adevarata sau falsa cu !      */
+/* -------------------------------------------- */
+const text = "text";
+console.log("Textul este string?", typeof text != "number");
