642c9d1d2a59c62e35a73867f4527759c64acaf9
surfthecode <surfthecode@gmail.com>
2023-05-30 12:22:13 +0300

    ex3 done

--- script/script-s18-ex-docs.js
+++ script/script-s18-ex-docs.js
@@ -30,3 +30,55 @@ Array.prototype.filterEven = function () {
 
 const newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(newArr.filterEven());
+
+console.log("---------------Exercise 3---------------");
+// Creați o clasă "FormaGeometrica" care să aibă o metodă statică "descriere" care afișează o descriere generală a formei geometrice. Extindeți clasa pentru a crea subclase pentru forme geometrice specifice, cum ar fi "Cerc" și "Patrat". În fiecare subclasă, adăugați metode specifice pentru calculul ariei și a perimetrului și utilizați keyword-ul "super" pentru a accesa metodele și proprietățile clasei părinte.
+class geometricShape {
+  static description() {
+    console.log(`This is a geometric shape...`);
+  }
+}
+
+class Circle extends geometricShape {
+  constructor(r) {
+    super();
+    this.r = r;
+  }
+
+  calcArea() {
+    return Math.PI * Math.pow(this.r, 2);
+  }
+
+  calcPerimeter() {
+    return 2 * Math.PI * this.r;
+  }
+}
+
+class Square extends geometricShape {
+  constructor(l) {
+    super();
+    this.l = l;
+  }
+
+  calcArea() {
+    return Math.pow(this.l, 2);
+  }
+
+  calcPerimeter() {
+    return 4 * this.l;
+  }
+}
+
+const newCircle = new Circle(5);
+console.log(
+  `The area of your circle is ${Math.floor(
+    newCircle.calcArea()
+  )}\nThe periemter of your circle is ${Math.floor(newCircle.calcPerimeter())}`
+);
+
+const newSquare = new Square(5);
+console.log(
+  `The area of your Square is ${Math.floor(
+    newSquare.calcArea()
+  )}\nThe periemter of your Square is ${Math.floor(newSquare.calcPerimeter())}`
+);
