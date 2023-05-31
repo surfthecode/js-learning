7e0ec0c2cfb3c662bf873863bd90511d7aa8ea87
surfthecode <surfthecode@gmail.com>
2023-05-31 11:21:29 +0300

    ex1 done

---
+++ script/script-s19-ex-docs.js
@@ -0,0 +1,9 @@
+console.log("---------------Exercise 1---------------");
+// Creați un element HTML cu un anumit ID. Adăugați un eveniment de clic la acest element. În funcția de tratare a evenimentului de clic, schimbați culoarea de fundal a elementului.
+const ex1Btn = document.getElementById("ex1");
+ex1Btn.addEventListener("click", function () {
+  ex1Btn.style.backgroundColor = "black";
+});
+
+console.log("---------------Exercise 2---------------");
+// Creați un element HTML cu un anumit ID. Adăugați un eveniment de "mouseover" (trecerea cursorului peste element) la acest element. În funcția de tratare a evenimentului de "mouseover", adăugați o clasă CSS la element folosind classList.add. Adăugați și un eveniment de "mouseout" (ieșirea cursorului din element) la același element. În funcția de tratare a evenimentului de "mouseout", eliminați clasa CSS folosind classList.remove.
