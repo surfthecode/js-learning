7ca89f3ae238499b1ad4ba39b6d061f84f928bf2
surfthecode <surfthecode@gmail.com>
2023-06-06 17:52:24 +0300

    start AJAX exercise using fetch

--- script/script-s20-ajax-practice.js
+++ script/script-s20-ajax-practice.js
@@ -1,25 +1,34 @@
-function showCountries() {
-  const xhr = new XMLHttpRequest();
+// Using XMLHttpRequest
 
-  xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+// const showCountries = function () {
+//   const xhr = new XMLHttpRequest();
 
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      let countries = JSON.parse(this.response);
-      console.log(countries);
-      countries.forEach((country) => {
-        const countryCard = document.createElement("div");
-        const countryCardImage = document.createElement("img");
+//   xhr.open("GET", "https://restcountries.com/v3.1/all", true);
 
-        countryCard.innerHTML = country.name.common;
+//   xhr.onload = function () {
+//     if (xhr.status !== 200)
+//       alert("Attention required: status not 200, check response status!");
+//     else {
+//       let countries = JSON.parse(this.response);
 
-        countryCardImage.src = country.flags.svg;
-        countryCard.appendChild(countryCardImage);
+//       countries.forEach((country) => {
+//         const countryCard = document.createElement("div");
+//         const countryCardImage = document.createElement("img");
 
-        document.getElementById("parentDiv").appendChild(countryCard);
-      });
-    }
-  };
+//         countryCard.innerHTML = country.name.common;
+//         countryCardImage.src = country.flags.svg;
+//         countryCard.appendChild(countryCardImage);
 
-  xhr.send();
-}
+//         document.getElementById("parentDiv").appendChild(countryCard);
+//       });
+//     }
+//   };
+
+//   xhr.send();
+// };
+
+// Using Fetch API
+
+fetch("https://restcountries.com/v3.1/all")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
