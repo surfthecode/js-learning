e86c21268d913519b067440639e0f2bb66a66bfd
surfthecode <surfthecode@gmail.com>
2023-06-12 13:48:03 +0300

    s20 ajax ex1 done

--- script/script-s20-disc.js
+++ script/script-s20-disc.js
@@ -6,3 +6,32 @@ console.log("---------------Exercise 1---------------");
 // 1. Exercițiul 1: Afișarea tuturor băuturilor
 //    - Parcurge fiecare element din fișierul JSON și afișează numele brandului și aroma fiecărei băuturi în div-ul cu id-ul "paragraph".
 const btn = document.querySelector("#cta-primary");
+const div = document.querySelector(".paragraph");
+
+btn.addEventListener("click", function () {
+  let xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "./JSON/soda.json", true);
+
+  xhr.onload = function () {
+    if (xhr.status !== 200) {
+      throw new Error("Error status..");
+    } else {
+      let sodas = JSON.parse(this.response);
+
+      sodas.forEach((soda) => {
+        console.log(soda);
+        const brandName = soda.brand;
+        const flavor = soda.flavor;
+        const newDiv = document.createElement("div");
+
+        newDiv.innerHTML = `Drink ${soda.id}: ${brandName} - flavor: ${flavor}`;
+        newDiv.style.color = "antiquewhite";
+
+        div.appendChild(newDiv);
+      });
+    }
+  };
+
+  xhr.send();
+});
