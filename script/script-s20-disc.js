8e63f65346401b154b1d510e623ea3931636b81e
surfthecode <surfthecode@gmail.com>
2023-06-12 13:10:28 +0300

    create files for s20 exercises on discord

---
+++ script/script-s20-disc.js
@@ -0,0 +1,8 @@
+// Prerequisites:
+//    - Creați un buton cu id-ul "cta-primary" și un div cu id-ul "paragraph".
+//    - La apăsarea butonului, utilizați Ajax pentru a încărca conținutul din fișierul "soda.json" (inițial poți folosi console.log()).
+
+console.log("---------------Exercise 1---------------");
+// 1. Exercițiul 1: Afișarea tuturor băuturilor
+//    - Parcurge fiecare element din fișierul JSON și afișează numele brandului și aroma fiecărei băuturi în div-ul cu id-ul "paragraph".
+const btn = document.querySelector("#cta-primary");
